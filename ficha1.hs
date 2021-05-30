79ac494369ce0d021fdd43e49524307af11215eb
Hugo Ferreira <40798729+hf97@users.noreply.github.com>
2021-05-30 18:27:39 +0100

    Final fich1

--- ficha1.hs
+++ ficha1.hs
@@ -19,46 +19,44 @@ data Bexp = T Bool
           | Or Bexp Bexp
         deriving (Show,Eq,Ord)
 
-funcA :: Aexp -> State -> Int
-funcA (C a1) s = a1
-funcA (V x) s = get x s
-funcA (Soma a1 a2) s = (funcA a1 s) + (funcA a2 s)
-funcA (Mult a1 a2) s = (funcA a1 s) * (funcA a2 s)
-funcA (Sub a1 a2) s = (funcA a1 s) - (funcA a2 s)
+evalA :: Aexp -> State -> Int
+evalA (C a1) s = a1
+evalA (V x) s = get x s
+evalA (Soma a1 a2) s = (evalA a1 s) + (evalA a2 s)
+evalA (Mult a1 a2) s = (evalA a1 s) * (evalA a2 s)
+evalA (Sub a1 a2) s = (evalA a1 s) - (evalA a2 s)
 
-funcB :: Bexp -> State -> Bool
-funcB (T True) s = True
-funcB (F False) s = False
-funcB (Eq a1 a2) s = (funcA a1 s) == (funcA a2 s)
-funcB (Leq a1 a2) s = (funcA a1 s) <= (funcA a2 s)
-funcB (Not a1) s = not(funcB a1 s)
-funcB (And b1 b2) s = funcB b1 s && funcB b2 s
-funcB (Or b1 b2) s = funcB b1 s || funcB b2 s
+evalB :: Bexp -> State -> Bool
+evalB (T True) s = True
+evalB (F False) s = False
+evalB (Eq a1 a2) s = (evalA a1 s) == (evalA a2 s)
+evalB (Leq a1 a2) s = (evalA a1 s) <= (evalA a2 s)
+evalB (Not a1) s = not(evalB a1 s)
+evalB (And b1 b2) s = evalB b1 s && evalB b2 s
+evalB (Or b1 b2) s = evalB b1 s || evalB b2 s
 
 
 data Stm = Ass Var Aexp
          | Skip
---  //TODO a lista tem de ser de Stm, Aexp ou Bexp
          | Comp [Stm]
          | If Bexp Stm Stm
          | While Bexp Stm
         deriving (Show,Eq,Ord)
 
 evalNS :: Stm -> State -> State
--- evalNS (Ass x a) s = (x,funcA a s):s
 evalNS (Ass x a) s = update x a s
 evalNS Skip s = s
 evalNS (Comp []) s = s
 evalNS (Comp (s1:s2)) s = evalNS (Comp s2) (evalNS s1 s) 
-evalNS (If b s1 s2) s | funcB b s == True = evalNS s1 s
+evalNS (If b s1 s2) s | evalB b s == True = evalNS s1 s
                       | otherwise = evalNS s2 s
-evalNS (While b s1) s | funcB b s == True = evalNS (Comp [s1, While b s1]) s
+evalNS (While b s1) s | evalB b s == True = evalNS (Comp [s1, While b s1]) s
                       | otherwise = s
 
 
 update :: Var -> Aexp -> State -> State
 update v a s = [(v, z)] ++ filter (\(key, value) -> key /= v) s
-               where z = funcA a s
+               where z = evalA a s
 
 get :: Var -> State -> Int
 get v ((key,value):t) | v == key = value
@@ -67,11 +65,11 @@ get v ((key,value):t) | v == key = value
 
 main = do
         putStr "\nVerificar valor de variavel no estado\n"
-        print (funcA (V "x") [("x",1)])
+        print (evalA (V "x") [("x",1)])
         putStr "\nAtribuição x=1: \n"
         print (evalNS (Ass "x" (C 1)) [])
         putStr "\nValidar x == y and True, no estado em que x=1 e y=1:\n"
-        print (funcB (And ((Eq (V "x") (V "y"))) (T True)) [("x",1),("y",1)])
+        print (evalB (And ((Eq (V "x") (V "y"))) (T True)) [("x",1),("y",1)])
         putStr "\nComp x=1;y=3\n"
         print (evalNS (Comp [(Ass "a" (C 1)), (Ass "y" (C 3))]) [])
         putStr "\nx=1; y=3; if x<=y then x=y else skip\n"
